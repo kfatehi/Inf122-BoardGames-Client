d6e455b9ced3a6757b256226bd2c5b24d9c5944b
Kyle San Clemente <kyle@oomba.com>
2017-03-13 01:28:33 -0700

    added margin and clearfix to page

--- app/components/Page.jsx
+++ app/components/Page.jsx
@@ -4,11 +4,16 @@ import * as actionCreators from '../action-creators';
 import { ConnectForm } from './ConnectForm.jsx';
 import { PlayerProfile } from './PlayerProfile.jsx';
 import { Link } from 'react-router';
+import FullWidth from './FullWidth.jsx';
 import Favicon from 'react-favicon';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import Paper from 'material-ui/Paper';
+import ClearFix from 'material-ui/internal/ClearFix';
+import spacing from 'material-ui/styles/spacing';
 
 const PageComponent = React.createClass({
   render: function () {
@@ -25,10 +30,9 @@ const PageComponent = React.createClass({
       disconnect,
       toServer
     } = this.props;
-    const disconnectLink = () => <a href="#" onClick={disconnect}>Disconnect</a>
     return <div>
       <Favicon url="http://findicons.com/files/icons/1786/oxygen_refit/128/package_games_board.png" />
-      { !connected && !connecting ? <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      {!connected && !connecting ? <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
           <ConnectForm />
         </MuiThemeProvider> : null}
 
@@ -36,18 +40,25 @@ const PageComponent = React.createClass({
           <MuiThemeProvider>
             <AppBar
               title="Bored Games - INF122 Final Project "
-              iconClassNameRight="muidocs-icon-navigation-expand-more"
+              iconElementRight={<FlatButton label="Disconnect" onTouchTap={disconnect}/>}
             />
           </MuiThemeProvider>
-
-          <div id='content'>
-            Connected {username}@{server} {disconnectLink()}
-            { myProfile ?
-                <PlayerProfile username={username} games={myProfile.games}/>
-                : null
-            }
-            {this.props.children}
-          </div>
+          <MuiThemeProvider>
+            <ClearFix style={{padding: spacing.desktopGutter, boxSizing: 'border-box'}}>
+              <div id='content' style={{maxWidth: 1200, margin: '0 auto',}}>
+                { myProfile ?
+                    <PlayerProfile username={username} games={myProfile.games}/>
+                    : null
+                }
+                {this.props.children}
+              </div>
+            </ClearFix>
+          </MuiThemeProvider>
+          <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <Paper style={{bottom: 0, position: 'absolute'}}>
+              Connected {username}@{server}
+            </Paper>
+          </MuiThemeProvider>
         </div> : null}
     </div>
   }
