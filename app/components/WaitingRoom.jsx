065964a6e8da92f73f485c55d7eac39b63929275
Kyle San Clemente <kyle@oomba.com>
2017-03-14 11:56:21 -0700

    made the spinner bigger and fixed the header, will style more later

--- app/components/WaitingRoom.jsx
+++ app/components/WaitingRoom.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import * as actionCreators from '../action-creators';
+import CircularProgress from 'material-ui/CircularProgress';
 
 export const WaitingRoomComponent = React.createClass({
   render() {
     const { id, openPug, imJoined, need, joinGame } = this.props;
     return <div>
       { openPug ? <div>
-        <h3>{openPug.pugName}</h3>
-        <p>{openPug.name}</p>
-        <p>Needs {need} more player{need === 1 ? '' : 's'}.</p>
-        { imJoined ? null : <button onClick={()=>joinGame(id)}>Join</button> }
+        <h2>Waiting Room: <span style={{color: '#00BCD4'}}>{openPug.pugName}</span></h2>
+        <p>Game: {openPug.name}</p>
+        <p>Waiting for {need} more player{need === 1 ? '' : 's'}.</p>
+        <CircularProgress size={80} thickness={5} />
+        { imJoined ? null : <span>{joinGame(id)}</span>}
       </div> : <div>
       <p>This is not an open game. It may be full or not exist.</p>
       <p><Link to="/">Click here to go back home</Link></p>
@@ -30,4 +32,4 @@ function mapStateToProps(state, props) {
   return { id, openPug, imJoined, need };
 }
 
-export const WaitingRoom = connect(mapStateToProps, actionCreators)(WaitingRoomComponent); 
+export const WaitingRoom = connect(mapStateToProps, actionCreators)(WaitingRoomComponent);
